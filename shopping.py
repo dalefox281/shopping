8d9a86a5416cc7a92b2d8088af2a5dbff651f651
Dale <dale.a.lenze@gmail.com>
2025-04-12 16:18:20 -0500

    Added comment to script

--- shopping.py
+++ shopping.py
@@ -1,5 +1,7 @@
 #!/bin/usr/env python3
 
+# Recipes could be innate but could also maybe input your own to add to the db.
+
 shepherdsPie = {'ingredients': ['Beef', 'Frozen Peas & Carrots', 'Onion', 'Potatoes', 'Shredded Cheddar'], 
                 'preferred_cook': 'Dad',
                 'difficulty': 'Medium',}
