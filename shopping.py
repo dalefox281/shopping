6ac96222538ad14e8a86de2c875f3b0689e0bc4b
Dale <dale.a.lenze@gmail.com>
2025-04-12 16:10:11 -0500

    Add initial script for shopping application

---
+++ shopping.py
@@ -0,0 +1,5 @@
+#!/bin/usr/env python3
+
+shepherdsPie = {'ingredients': ['Beef', 'Frozen Peas & Carrots', 'Onion', 'Potatoes', 'Shredded Cheddar'], 
+                'preferred_cook': 'Dad',
+                'difficulty': 'Medium',}
